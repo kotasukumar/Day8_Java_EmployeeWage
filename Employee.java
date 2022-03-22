ca38ad53e3fb7f9498cce04f7e07534f6a920c2c
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-03-22 19:13:45 +0530

    added:for loop

--- Employee.java
+++ Employee.java
@@ -2,29 +2,37 @@ package com.company;
 
 public class Employee {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
 
         int PER_HOUR_WAGE = 20;
         int WORKING_HOURS = 0;
         final int IS_FULL_TIME_EMP = 1;
         final int IS_PART_TIME_EMP = 2;
-        int random = (int) Math.floor(Math.random() * 10) % 3;
-        switch (random) {
+        int monthly_hours = 0;
+        int total_working_days = 1;
 
-            case IS_FULL_TIME_EMP:
-                WORKING_HOURS = 8;
-                System.out.println("Full time employee is present");
-                break;
-            case IS_PART_TIME_EMP:
-                WORKING_HOURS = 8;
-                System.out.println("Part time employee is present");
-                break;
-            default:
-                WORKING_HOURS = 0;
-                System.out.println("Employee is absent");
+        for(int i = 0; i < 21; i++){
+            int random = (int) Math.floor(Math.random() * 10) % 3;
+            switch (random) {
+
+                case IS_FULL_TIME_EMP:
+                    WORKING_HOURS = 8;
+                    System.out.println("Full time employee is present");
+                    total_working_days++;
+                    break;
+                case IS_PART_TIME_EMP:
+                    WORKING_HOURS = 8;
+                    System.out.println("Part time employee is present");
+                    total_working_days++;
+                    break;
+                default:
+                    WORKING_HOURS = 0;
+                    System.out.println("Employee is absent");
+            }
+            monthly_hours = monthly_hours + WORKING_HOURS;
         }
-        int daily_wage = PER_HOUR_WAGE * WORKING_HOURS;
-        System.out.println("today wage is : " + daily_wage);
+        int monthly_wage = PER_HOUR_WAGE * monthly_hours;
+        System.out.println("monthly wage is: " + monthly_wage);
     }
 }
