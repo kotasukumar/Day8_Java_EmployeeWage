fdc58575f104b1f9245fc6ffb516ae33f75c5f0a
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-03-22 18:39:02 +0530

    added:Attendence logic

--- Employee.java
+++ Employee.java
@@ -3,5 +3,13 @@ package com.company;
 public class Employee {
     public static void main(String[] args){
         System.out.println("Welcome to Employee Wage Computation Program");
+
+        double random = Math.floor(Math.random() * 10) % 2;
+        if(random == 1){
+            System.out.println("Employee is present");
+        }
+        else{
+            System.out.println("Employee is absent");
+        }
     }
 }
