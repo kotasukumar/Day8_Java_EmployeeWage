62ccf46648b1dacb8542d3a420240800f8f921f5
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-03-22 19:19:16 +0530

    added:Class and object

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Employee Wage Computation Program ");
+
+        EmployeeWage employeeWage = new EmployeeWage();
+        employeeWage.wagePerHour = 20;
+        employeeWage.Calculation();
+    }
+}
