b7a674d62d5bab38e1322d68c51adbc7abf053c7
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-03-22 18:43:44 +0530

    added:Daily wage logic

--- Employee.java
+++ Employee.java
@@ -1,15 +1,23 @@
 package com.company;
 
 public class Employee {
+
     public static void main(String[] args){
         System.out.println("Welcome to Employee Wage Computation Program");
+        
+        int PER_HOUR_WAGE = 20;
+        int WORKING_HOURS = 0;
 
         double random = Math.floor(Math.random() * 10) % 2;
         if(random == 1){
+            WORKING_HOURS = 8;
             System.out.println("Employee is present");
         }
         else{
+            WORKING_HOURS = 0;
             System.out.println("Employee is absent");
         }
+        int daily_wage = PER_HOUR_WAGE * WORKING_HOURS;
+        System.out.println("today wage is: " + daily_wage);
     }
 }
